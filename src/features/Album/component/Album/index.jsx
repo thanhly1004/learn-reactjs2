2697ef964607278afc1779d5a70f6c4f41ae4d0e
Thanh Ly <doly73073@gmail.com>
2021-10-09 15:34:11 +0700

    Album

---
+++ src/features/Album/component/Album/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+Album.propTypes = {
+    
+    album:PropTypes.array.isRequired
+};
+
+function Album({album}) {
+    return (
+        <div className="album">
+            <div className="album__thumbnail">
+                    <img src={album.thumbnailUrl} alt="" />
+            </div>
+            <p className="album__name">{album.name}</p>
+        </div>
+    );
+}
+
+export default Album;
